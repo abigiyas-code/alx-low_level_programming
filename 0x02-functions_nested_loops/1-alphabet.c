d9be8363212b602bc1f0d3d9597824471f58c3a5
Abigiya Fikremariam <abigiya.fikremariam@yahoo.com>
2022-06-05 10:25:33 +0300

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+char j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - Computes the absolute value of an integer
+ *Description: number being tested
+ *@n:integer n
+ *Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+return (0);
+}

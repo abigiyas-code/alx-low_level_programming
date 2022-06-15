971b0c2756daab35e8dc3cc7bb59945bf7e45193
Abigiya Fikremariam <51966855+abigiyas-code@users.noreply.github.com>
2022-06-15 21:14:26 +0300

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * _strcat - Concatenates two strings
+ * _strcat - Concatenates two strings 
  * @dest: The destination string
  * @src: The source string
+ *
  *
  * Return: A pointer to the resulting string dest
  */

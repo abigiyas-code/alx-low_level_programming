a3ec26b987197465f9cc439255d7f7f776d1368b
Abigiya Fikremariam <51966855+abigiyas-code@users.noreply.github.com>
2022-06-20 04:49:35 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,38 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* *cap_string - capitalize words
-* @str: pointer
-* Return: capitalzied string
-*/
-
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
 char *cap_string(char *str)
 {
-char sep[] = ",\t;\n; .!?\"(){}";
-int flag, i, ii;
-for (i = 0; str[i] != '\0'; i++)
+int index = 0;
+
+while (str[index])
 {
-flag = 0;
-if (i == 0)
-{
-flag = 1;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+
+index++;
 }
-else
-{
-for (ii = 0; sep[ii] != '\0'; ii++)
-{
-if (str[i - 1] == sep[ii])
-{
-flag = 1;
-break;
-}
-}
-{
-if (flag == 1)
-{
-if (str[i] <= 'z' && str[i] >= 'a')
-{
-str[i] -= ('a' - 'A');
-}
-}
-}
+
 return (str);
 }

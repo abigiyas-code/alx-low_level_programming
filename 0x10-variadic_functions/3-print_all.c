a4e04dc4133bfbf39b9f197af88f7bd0e585b75f
Abigiya Fikremariam <51966855+abigiyas-code@users.noreply.github.com>
2022-07-14 18:38:44 +0300

    print char, float, int

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
-<include "variadic constants.h">
-/** 
-print_all - prints anything.
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
  *
  * Return: no return.
